e2b44153e56cb6956010e827648b1d7cc035c945
LI Yu <liyu1981@gmail.com>
2021-12-20 16:14:35 +1100

    Add src/toolsData/manifest.js

---
+++ src/toolsData/manifest.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "Manifest",
+  "description": "Manifest is a grid-based pinboard for note taking.",
+  "url": "https://www.manifest.app/",
+  "favicon": "https://www.manifest.app/favicon.d42470cd.ico",
+  "canUseWithIframe": false
+};
