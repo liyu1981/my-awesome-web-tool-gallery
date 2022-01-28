9d925a3cbe46725896081e3002a3919d5139c8e3
LI Yu <liyu1981@gmail.com>
2022-01-28 23:48:11 +1100

    Add src/toolsData/colormind.js

---
+++ src/toolsData/colormind.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "Colormind",
+  "description": "Colormind - Bootstrap UI colors",
+  "url": "http://colormind.io/bootstrap/",
+  "favicon": "http://colormind.io/favicon32.gif",
+  "canUseWithIframe": false
+};
