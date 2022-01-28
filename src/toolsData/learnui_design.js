3343aab83486044266c4a347dd7bd1e79fcc90af
LI Yu <liyu1981@gmail.com>
2022-01-28 23:50:55 +1100

    Add src/toolsData/learnui_design.js

---
+++ src/toolsData/learnui_design.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "Learnui Color Generator",
+  "description": "Data Viz Color Palette Generator (for Charts & Dashboards)",
+  "url": "https://learnui.design/tools/data-color-picker.html#palette",
+  "favicon": "https://learnui.design/favicon.ico",
+  "canUseWithIframe": false
+};
