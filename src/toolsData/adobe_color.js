560485c2479ed5d90abdf27fc53bda0f7ad7b81e
LI Yu <liyu1981@gmail.com>
2022-02-24 10:44:53 +1100

    Add src/toolsData/adobe_color.js

---
+++ src/toolsData/adobe_color.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "Adobe Color",
+  "description": "Color wheel, a color palette generator | Adobe Color",
+  "url": "https://color.adobe.com/create/color-wheel",
+  "favicon": "https://color.adobe.com/favicon.ico",
+  "canUseWithIframe": false
+};
