41f527e9998a8d17ef6b0d4614eb21a321e8a461
LI Yu <liyu1981@gmail.com>
2022-03-08 15:28:49 +1100

    Add src/toolsData/excalidraw.js

---
+++ src/toolsData/excalidraw.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "name": "Excalidraw",
+  "description": "Excalidraw | Hand-drawn look & feel, Collaborative, Secure",
+  "url": "https://excalidraw.com/",
+  "favicon": "https://excalidraw.com/favicon.ico",
+  "canUseWithIframe": false
+};
